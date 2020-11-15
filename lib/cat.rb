44364bc0472aa7df71b436fdbb1dad638976958a
David Moina <davemolina777@gmail.com>
2020-11-15 09:04:27 -0600

    commit tree 3bf9d6e4e220d29a1111e6da64fcce4c824ac833

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
   # code goes here
+  @@all = []
+  attr_accessor :mood, :owner
+  attr_writer 
+  attr_reader :name
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @mood = mood
+    @owner = owner
+    @@all << self
+  end
+  def self.all
+  @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
+require "pry"
 class Owner
   # code goes here
+  @@all = []
+  attr_accessor :dog, :cat
+  attr_writer 
+  attr_reader :name, :species
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    save
+  end
+  def say_species
+    "I am a #{@species}."
+  end
+  def save
+  @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.length
+  end
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    #binding.pry
+    Cat.all.find_all do |cat|
+      cat.owner == self
+    end
+  end
+  def dogs
+    Dog.all.find_all do |dog|
+      dog.owner == self
+    end
+  end
+  def buy_cat(name)
+    #binding.pry
+    Cat.new(name, self)
+   end
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  def walk_dogs
+    self.dogs.collect {|dog| dog.mood = "happy"}
+  end
+  def feed_cats
+    self.cats.collect {|cat| cat.mood = "happy"}
+  end
+  def sell_pets
+  
+    self.cats.collect {|cat| cat.mood = "nervous"}
+    self.dogs.collect {|dog| dog.mood = "nervous"}
+    self.dogs.collect {|dog| dog.owner = nil}
+    self.cats.collect {|cat| cat.owner = nil}
+  end
+  def list_pets
+   # binding.pry
+    "I have #{self.dogs.length} dog(s), and #{self.cats.length} cat(s)."
+  end
+
 end
